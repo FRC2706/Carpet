310af0aca34de16a6585e6622ad1fb79c1315313
Ethan Tovell <erwtovell@gmail.com>
2019-01-19 11:36:42 -0500

    Added ability to connect to ftp server

--- app/src/main/java/mergerobotics/memo/backend/App.java
+++ app/src/main/java/mergerobotics/memo/backend/App.java
@@ -1,4 +1,25 @@
 package mergerobotics.memo.backend;
 
-public class App {
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by mike on 09/10/16.
+ * This is a helper class to be able to get the App's Context without needing to pass around pointers to MainActivity.
+ */
+
+public class App extends Application {
+
+    private static Context mContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = this;
+    }
+
+    public static Context getContext(){
+        return mContext;
+    }
 }
