b88542dad4d5a2c64748581d089d129d82ce506e
Alden Peeters <starbounder1024@gmail.com>
2019-02-16 10:25:50 -0500

    Added Chaquopy stuff

--- app/src/main/java/mergerobotics/memo/MainActivity.java
+++ app/src/main/java/mergerobotics/memo/MainActivity.java
@@ -11,68 +11,69 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+
 import mergerobotics.memo.gui.prematchActivity;
 import mergerobotics.memo.gui.test;
 
 public class MainActivity extends AppCompatActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		
+		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+		fab.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Snackbar.make(view, "Coming soon...", Snackbar.LENGTH_LONG)
+						.setAction("Action", null).show();
+			}
+		});
+		
+		if(!Python.isStarted()) {
+			Python.start(new AndroidPlatform(this));
+			Python py = Python.getInstance();
+			PyObject mod = py.getModule("main");
+		}
+	}
+	
+	public void changePage(View view){
+		Intent name = new Intent(this, prematchActivity.class);
+		startActivity(name);
+	}
+	
+	public void changeTestPage(View view){
+		Intent name = new Intent(this, test.class);
+		startActivity(name);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Coming soon...", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-
-
-    }
-
-
-
-
-    public void changePage(View view){
-        Intent name = new Intent(this, prematchActivity.class);
-        startActivity(name);
-    }
-
-    public void changeTestPage(View view){
-        Intent name = new Intent(this, test.class);
-        startActivity(name);
-    }
-
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		
+		return super.onOptionsItemSelected(item);
+	}
 }
 
 
