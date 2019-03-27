d9b8c60b2722db4d2c9db6cf8d36b7a116c0a98f
Ethan Tovell <erwtovell@gmail.com>
2019-03-26 23:11:00 -0400

    Just backing up my work, work on notes

--- app/src/main/java/mergerobotics/memo/gui/TeamInfo.java
+++ app/src/main/java/mergerobotics/memo/gui/TeamInfo.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import mergerobotics.memo.R;
+import android.view.View;
+import android.widget.TextView;
 
 public class TeamInfo extends AppCompatActivity {
 
@@ -11,5 +13,8 @@ public class TeamInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_info);
+
+        TextView notes = (TextView)findViewById(R.id.textViewNotes);
+        notes.setText("No Notes To Display!");
     }
 }
